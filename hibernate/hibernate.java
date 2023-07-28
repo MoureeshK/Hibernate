a5421c9f2bcff909b27ed730884b077109150910
MoureeshK <122192520+MoureeshK@users.noreply.github.com>
2023-07-28 10:32:22 +0530

    Add files via upload

---
+++ hibernate/hibernate.java
@@ -0,0 +1,14 @@
+package com.hibernate;
+
+
+public class hibernate{
+	public static void main(String[] args) {
+		Employee obj = new Employee();
+		obj.setId(1);
+		obj.setName("Raja");
+		obj.setAge(24);
+		
+		
+		
+	}
+}
